103bf7ed0ba624e40c26c4967dc934aa02ec480b
moshiuzzaman <md.shozon041@gmail.com>
2020-11-27 14:04:09 +0600

    all complete

--- src/FakeData/FakeData.js
+++ src/FakeData/FakeData.js
@@ -6,12 +6,12 @@ var FakeDate=[
     },{
         'id':2,
         'name':"Apartment in lost Panorama", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(20)" ,'price':34,
-        "hotelImage":"http://gg.gg/n8unl"
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597826_XL.jpg?frz-v=21"
 
     },{
         'id':3,
         'name':"AR nLounge & Poll (r&r + b&b)", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(25)" ,'price':70,
-        "hotelImage":"http://gg.gg/n8uod"        
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597828_XL.jpg?frz-v=21"        
     },
 ]},
     {"id":2,"name":"Sreemangal","description":"Srimangal is the tea capital of Bangladesh. This hilly area, with tea estates, lemon orchards, pineapple, and rubber plantations, is one of the most picturesque and enjoyable parts of the country. For miles around, tea estates form a perennially green carpet on the sloping hills, and it is the one area (besides the Sundarbans) where it is possible to look around and not see another human being. A few days spent around its tea estates and exploring nearby villages and forests will almost certainly rank among your most treasured experiences in Bangladesh. ","sliderImage":"https://img.locationscout.net/images/2017-01/sreemangal-bangladesh-1j57_l.jpeg","background":"https://www.novemecoresort.com/wp-content/uploads/2018/12/3.jpg", 'hotel':[{
@@ -21,12 +21,12 @@ var FakeDate=[
     },{
         'id':2,
         'name':"Apartment in lost Panorama", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(20)" ,'price':34,
-        "hotelImage":"http://gg.gg/n8unl"
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597826_XL.jpg?frz-v=21"
 
     },{
         'id':3,
         'name':"AR nLounge & Poll (r&r + b&b)", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(25)" ,'price':70,
-        "hotelImage":"http://gg.gg/n8uod"        
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597828_XL.jpg?frz-v=21"        
     }]},
     {"id":3,"name":"Sundorbon","description":" The Sundarbans is the largest mangrove forest in the world. It has been declared the 52nd World Heritage Site in the world. The Sundarbans is in south west part of Bangladesh in the district of greater Khulna . India shares a bit of the forest with Bangladesh. The Sundarbans is a part of the world’s largest delta formed by the rivers Ganges, Brahmaputra and Meghna on the Bay of Bengal. Thousands of streams, creeks, rivers and estuaries have enhanced its charm. The total area of the forest is about 38,000 square kilo meters. The name may have been derived from the Sundari trees that are found in Sundarbans in large number. ","sliderImage":"https://dailyasianage.com/library/1582060099_9.jpg","background":"https://dailyasianage.com/library/1582060099_9.jpg", 'hotel':[{
         'id':1,
@@ -35,12 +35,12 @@ var FakeDate=[
     },{
         'id':2,
         'name':"Apartment in lost Panorama", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(20)" ,'price':34,
-        "hotelImage":"http://gg.gg/n8unl"
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597826_XL.jpg?frz-v=21"
 
     },{
         'id':3,
         'name':"AR nLounge & Poll (r&r + b&b)", 'details':"4 guests 2 bedrooms 2 beds 2 barhs Wif Air conditioning Kitchen","cancellation":"available",'star':"4.9(25)" ,'price':70,
-        "hotelImage":"http://gg.gg/n8uod"        
+        "hotelImage":"https://medias.bestwestern.fr/props_iceportal/6084/67597828_XL.jpg?frz-v=21"        
     }]}
     
 ]
